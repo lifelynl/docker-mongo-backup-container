08eb617d3ab1a4f09c08fa9656ee5789c73ccfd9
Dirk Kok <dirk@lifely.nl>
2018-06-08 15:08:11 +0200

    feat(cron): use /etc/environment

--- run.sh
+++ run.sh
@@ -9,7 +9,6 @@ else
   echo "$SCHEDULE root /usr/bin/env bash /backup.sh &>> /var/log/cron.log" >> /etc/crontab
   echo "#" >> /etc/crontab
   touch /var/log/cron.log
-  env > /root/.docker_env # dump env to file so we're able to get vars back when running a script from cron
-  sed -i "/SCHEDULE/c\SCHEDULE='$SCHEDULE'" /root/.docker_env # we need quotes around the asterisks
-  /usr/bin/env cron -f
+  env > /etc/environment # dump env to file so we're able to get vars back when running a script from cron
+  cron -f
 fi
